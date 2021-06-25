d3fe114e0cc85da3ea24e489eb3e7677159b17ae
SamDeMario-lab <64508273+SamDeMario-lab@users.noreply.github.com>
2021-06-25 16:46:41 -0700

    Started script to make the plots

--- upf1_rrp6_intron_plots.R
+++ upf1_rrp6_intron_plots.R
@@ -7,9 +7,13 @@ library(ggplot2)
 upf1_rrp6_sig <- subset(upf1_rrp6_ratios, subset = upf1_rrp6_ratios$WT_vs_rrp6_pval<0.05)
 upf1_rrp6_sig <- subset(upf1_rrp6_ratios, subset = upf1_rrp6_ratios$WT_vs_upf1_pval<0.05)
 WT_vs_upf1 <-ggplot(data = upf1_rrp6_ratios, aes(x = upf1_avg_ratio, y = WT_avg_ratio)) +
-  geom_point()
+  geom_point() +
+  ggtitle("WT vs upf1 unspliced transcripts") +
+  xlab("upf1 intronic reads ratio") +
+  ylab("WT intronic reads ratio")
 WT_vs_rrp6 <-ggplot(data = upf1_rrp6_ratios, aes(x = rrp6_avg_ratio, y = WT_avg_ratio)) +
   geom_point() + 
   ggtitle("WT vs rrp6 unspliced transcripts") +
   xlab("rrp6 intronic reads ratio") +
   ylab("WT intronic reads ratio")
+print(WT_vs_upf1)
